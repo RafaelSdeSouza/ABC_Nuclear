7b8f623863ca673e8c50dc3e873670cb612a9afc
Rafael S. de Souza <rafael.2706@gmail.com>
2019-04-16 17:08:47 -0400

    :+1:

--- ABC_toy/R_version/toy.R
+++ ABC_toy/R_version/toy.R
@@ -1,5 +1,5 @@
 require(EasyABC)
-
+require(ggplot2)
 # Toy programm
 
 
@@ -7,13 +7,10 @@ require(EasyABC)
 #   COMPUTE PREDICTED VALUES: BLACK BOX
 # ======================================================================
 #     the numbers in the following equations are "hidden"
-
-
-
-model <- function(par){
-  val1 <- 1e-1*par[1] +
-          2e-1*par[2]^2.0 +
-          7e-1*par[3] +
+toy_model <- function(par){
+  val1 <- 1e-1*par[1]^4 +
+          2e-1*par[2]^3.0 +
+          7e-1*par[3]^2 +
           4e-1*par[4];
   val2 <- 3e-2*par[1]^4.0 +
           5e-2*par[2]^3.0 +
@@ -21,40 +18,44 @@ model <- function(par){
           1e-2*par[4];
   val3 <- 7e-3*par[1]^4.0 +
           1e-3*par[2]^3.0 +
-          4e-3*par[3]^2.0 +
+          4e-3*par[3]^2 +
           2e-3*par[4];
-  val4 <- 2e-4*par[1]^4.0 +
-          5e-4*par[2]^3.0 +
-          8e-4*par[3]^2.0 +
-          8e-4*par[4];
-  val5 <- 9e-5*par[1]^4.0 +
-          6e-5*par[2]^3.0 +
-          3e-4*par[3]^2.0 +
-          1e-4*par[4]
-  
-return(c(val1,val2,val3,val4,val5))  
+  val4 <- 2e-4*par[1]^4 +
+          5e-4*par[2]^3 +
+          8e-4*par[3]^2 +
+          8e-4*par[4]
+#S1 <- mean(c(val1,val2,val3,val4)) 
+#S2 <- sd(c(val1,val2,val3,val4))
+  return(c(log(val1),log(val2),log(val3),log(val4)))
 }
 
-data <- model(c(34,12,456,78))
-summarydata <- data
+# Observed data
+x1 = 34
+x2 = 12
+x3 = 456
+x4 = 78
 
-toy_prior <- list(c("unif",1e-1,1000),c("unif",1e-1,1000),
-                  c("unif",1e-1,1000),c("unif",1e-1,1000))
+obs_data <- toy_model(c(x1,x2,x3,x4))
+# Summary, just the data - no compression
+sum_stat_obs <- obs_data
 
-ABC_Marjoram_original <- ABC_mcmc(method="Marjoram", model=model, 
-                                prior=list(c("unif",1e-1,1000),c("unif",1e-1,1000),
-                                           c("unif",1e-1,1000),c("unif",1e-1,1000)), 
-                                summary_stat_target=summarydata, n_rec = 1e5)
+toy_prior <- list(c("unif",1e-1,500),c("unif",1e-1,500),
+                  c("unif",1e-1,500),c("unif",1e-1,500))
 
 
-ABC_Drovandi <-ABC_sequential(method="Drovandi", model=model, prior=toy_prior,
-                             nb_simul=5000, summary_stat_target=summarydata, tolerance_tab=0.5, c=0.7)
+# Fraction of acceptable data, measured via Euclidian distance between simulation and obs_data
+tolerance=c(1,0.75,0.5,0.25,0.1)
 
-ABC_Beaumont <- ABC_sequential(method="Beaumont", model=model, prior=list(c("unif",1e-1,1000),c("unif",1e-1,1000),
-                                                                        c("unif",1e-1,1000),c("unif",1e-1,1000)),
-                             nb_simul=1e4, summary_stat_target=summarydata, tolerance_tab=c(1.5,0.5))
-                             
+# Run Sequencial Approximate Bayesian Computation
+ABC_Beaumont <- ABC_sequential(method="Beaumont", model=toy_model,
+                             prior=toy_prior, nb_simul=1e4, summary_stat_target=sum_stat_obs,
+                             tolerance_tab=tolerance,n_cluster=4,use_seed=TRUE)
+
+# Plot histogram
+hist(ABC_Beaumont$param[,1])
+
+# Plot density 2D
 pdat <- as.data.frame(ABC_Beaumont$param)
 
-ggplot(data = pdat,aes(x = V1,y = V4)) +
+ggplot(data = pdat,aes(x = V1,y = V2)) +
   geom_density_2d()
