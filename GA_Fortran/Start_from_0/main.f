c0b46662779fa3d2bfda58e636bbee6a6b75f775
Rafael S. de Souza <rafael.2706@gmail.com>
2019-05-24 09:46:12 -0400

    :-)

--- GA_Fortran/Start_from_0/main.f
+++ GA_Fortran/Start_from_0/main.f
@@ -6,9 +6,9 @@ c==================================================================
 c==================================================================
       implicit  none
       integer    n,M
-      real*8     x(4)
-	  real*8     y(5), loss
-	  external loss
+	real*8     x(4)
+	real*8     y(5), loss
+	external loss
 
 	  x(1) = 34
 	  x(2) = 12
@@ -17,12 +17,16 @@ c==================================================================
 
 
 c     Now call toy
-      call toy(n,x,y)
-c
+	  call toy(n,x,y)
+
+c     Now call pikaia
+      call pikaia(loss,n,ctrl,x,f,status)
+
+c      
 
 c     Print the results
-      write(*,*) '      out: ',y
-	  write(*,*) '      loss: ',loss(y)
+	write(*,*) '      out: ',y
+	write(*,*) '      loss: ',loss(y)
 c
       end
 c***************************************************************
